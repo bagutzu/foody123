c21f7f81abded8c77331b1cc025f7d4599604706
Bag Raul Cristian <raulbag1996@gmail.com>
2016-08-21 00:30:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,16 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
+  
+  appName: 'Foody123',
+  publicServerURL: 'http://foody123.herokuapp.com/parse',
+  verifyUserEmails: true, // Enable email verification
+  emailAdapter: PostmarkAdapter({
+    apiKey: '2d9d65d1-248a-4037-a7cc-c83a3a215eee',
+    fromAddress: 'secretariat@raucris.ro',
+  }),
+});
+  
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
